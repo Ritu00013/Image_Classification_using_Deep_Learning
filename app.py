0210b7750c586e5cc15d1088feabe8710ae056a1
Rahul Bhardwaj <rahul.03111999@gmail.com>
2022-12-05 13:32:07 +0530

    denseNet working fine,CNN not predicting correctly

--- app.py
+++ app.py
@@ -5,16 +5,47 @@ import numpy as np
 
 app = Flask(__name__)
 
-cnnModel = load_model("models/CNN_Model.h5")
+# print(tf.__version__)
+BATCH_SIZE = 10
+AUTOTUNE = tf.data.experimental.AUTOTUNE
+cnnModel = load_model("models/DenseNet.h5")
 
-img = tf.keras.utils.load_img("parrot.jpg", target_size=(128, 128))
+ds_train_ = tf.keras.utils.image_dataset_from_directory(
+    "train",
+    labels='inferred',
+    #label_mode='binary',
+    image_size=[200, 200],
+    interpolation='nearest',
+    batch_size=BATCH_SIZE,
+    shuffle=True,
+)
+
+# Data Pipeline
+def convert_to_float(image, label):
+    image = tf.image.convert_image_dtype(image, dtype=tf.float32)
+    return image, label
+
+ds_train = (
+    ds_train_
+    .map(convert_to_float)
+    .cache()
+    .prefetch(buffer_size=AUTOTUNE)
+)
+
+img = tf.keras.utils.load_img("parrot.jpg", target_size=(200, 200))
 img_array = tf.keras.utils.img_to_array(img=img)
 img_array = tf.expand_dims(img_array, 0)
 
-predictions = cnnModel.predict(img_array)
-score = tf.nn.softmax(predictions)
+class_names = ds_train_.class_names
 
-print(predictions[0] * 100)
+predictions = cnnModel.predict(img_array)
+score = tf.nn.softmax(predictions[0])
+
+print(
+    "This image most likely belongs to {} with a {:.2f} percent confidence."
+    .format(class_names[np.argmax(score)], 100 * np.max(score))
+)
+
 
 
 @app.route("/", methods=["POST", "GET"])
