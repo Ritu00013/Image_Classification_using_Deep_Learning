3ba2f13d6436c68afde4c504f9d76ee0994c8fce
Rahul Bhardwaj <rahul.03111999@gmail.com>
2022-12-13 11:14:06 +0530

    denseNet working CNN not working

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 from flask import Flask, render_template, request, flash, redirect
+from flask_wtf import FlaskForm
+from wtforms import FileField, SubmitField
 import urllib.request
 import os
 from werkzeug.utils import secure_filename
@@ -8,8 +10,9 @@ import numpy as np
 
 UPLOAD_FOLDER = 'static/uploads/'
 
+
 app = Flask(__name__)
-app.secret_key = "akdschssjkdv"
+app.config['SECRET_KEY'] = 'sjdovbk'
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 app.config['MAX_DIMENSIONS'] = 16 * 1024 * 1024
 
@@ -17,47 +20,57 @@ ALLOWED_EXT = set(['png', 'jpg', 'jpeg'])
 def allowedFiles(filename):
     return "." in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXT
 
-# # print(tf.__version__)
-# BATCH_SIZE = 10
-# AUTOTUNE = tf.data.experimental.AUTOTUNE
-# cnnModel = load_model("models/DenseNet.h5")
 
-# ds_train_ = tf.keras.utils.image_dataset_from_directory(
-#     "train",
-#     labels='inferred',
-#     #label_mode='binary',
-#     image_size=[200, 200],
-#     interpolation='nearest',
-#     batch_size=BATCH_SIZE,
-#     shuffle=True,
-# )
+class UploadFileForm(FlaskForm):
+    file = FileField("File")
+    submit = SubmitField("Upload File")
 
-# # Data Pipeline
-# def convert_to_float(image, label):
-#     image = tf.image.convert_image_dtype(image, dtype=tf.float32)
-#     return image, label
+# print(tf.__version__)
+BATCH_SIZE = 10
+AUTOTUNE = tf.data.experimental.AUTOTUNE
 
-# ds_train = (
-#     ds_train_
-#     .map(convert_to_float)
-#     .cache()
-#     .prefetch(buffer_size=AUTOTUNE)
-# )
 
-# img = tf.keras.utils.load_img("parrot.jpg", target_size=(200, 200))
-# img_array = tf.keras.utils.img_to_array(img=img)
-# img_array = tf.expand_dims(img_array, 0)
+ds_train_ = tf.keras.utils.image_dataset_from_directory(
+    "train",
+    labels='inferred',
+    #label_mode='binary',
+    image_size=[200, 200],
+    interpolation='nearest',
+    batch_size=BATCH_SIZE,
+    shuffle=True,
+)
 
-# class_names = ds_train_.class_names
+# Data Pipeline
+def convert_to_float(image, label):
+    image = tf.image.convert_image_dtype(image, dtype=tf.float32)
+    return image, label
 
-# predictions = cnnModel.predict(img_array)
-# score = tf.nn.softmax(predictions[0])
+ds_train = (
+    ds_train_
+    .map(convert_to_float)
+    .cache()
+    .prefetch(buffer_size=AUTOTUNE)
+)
 
-# print(
-#     "This image most likely belongs to {} with a {:.2f} percent confidence."
-#     .format(class_names[np.argmax(score)], 100 * np.max(score))
-# )
+def findBird(filename):
+    print("i am here")
+    print(filename)
+    img = tf.keras.utils.load_img("static/uploads/"+filename, target_size=(200, 200))
+    img_array = tf.keras.utils.img_to_array(img=img)
+    img_array = tf.expand_dims(img_array, 0)
+    print(img_array)
+    class_names = ds_train_.class_names
+    denseNetModel = load_model("models/DenseNet.h5")
+    predictions = denseNetModel.predict(img_array)
+    score = tf.nn.softmax(predictions[0])
 
+    return(
+        "This bird is most likely a {} with a {:.2f} percent confidence."
+        .format(class_names[np.argmax(score)], 100 * np.max(score))
+    )
+
+
+# findBird("hawk.jpeg")
 
 
 @app.route("/", methods=["POST", "GET"])
@@ -96,21 +109,14 @@ def cnnModel():
 
 @app.route("/denseNetModel", methods=["POST", "GET"])
 def denseNetModel():
-    if 'file' not in request.files:
-        flash("No file! 😕")
-        return redirect(request.url)
-    file = request.files['file']
-    if file.filename == '':
-        flash("No image selected for uploading! 😕")
-        return redirect(request.url)
-    if file and allowedFiles(file.filename):
-        filename = secure_filename(file.filename)
-        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-        flash("Image uploaded successfully! 🚀")
-        return render_template("Densenet.html", filename=filename)
-    else:
-        flash("Allowed file types are: png, jpg, jpeg 😌")
-        return render_template("Densenet.html")
+    form = UploadFileForm()
+    if form.validate_on_submit():
+        file = form.file.data
+        file.save(os.path.join(os.path.abspath(os.path.dirname(__file__)), app.config['UPLOAD_FOLDER'], file.filename))
+        print("eyeyeyeyey")
+        text = findBird(file.filename)
+        return text
+    return render_template("Densenet.html", form=form)
 
 if __name__ == "__main__":
     app.run(debug=True)
